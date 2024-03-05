a2deaf6944b1bc2ea7f20f62d08ab43e2c1e6387
Thomas Tartière <thomas.tartiere@gmail.com>
2024-03-05 11:59:23 -0800

    fix: Invalid service config for docker

--- main-swarm-compute.tf
+++ main-swarm-compute.tf
@@ -1,71 +1,11 @@
-resource "azurerm_resource_group" "swarm_cluster" {
-  name     = "${local.product_key}-${local.environment_key}-${local.location_key}-swarm-rg"
-  location = local.location
-  tags     = local.tags
-}
 
-resource "azurerm_virtual_network" "swarm_cluster_vnet" {
-  name                = "${local.product_key}-${local.environment_key}-vnet-${local.location_key}${var.name_postfix}"
-  resource_group_name = azurerm_resource_group.swarm_cluster.name
-  location            = azurerm_resource_group.swarm_cluster.location
-  address_space       = ["10.254.0.0/16"] # /16 == 65,536 addresses
-  tags                = local.tags
-
-  #   lifecycle {
-  #     prevent_destroy = true
-  #   }
-}
-
-resource "azurerm_subnet" "node_subnet" {
-  name                 = "${local.product_key}-${local.environment_key}-subnet-backend-${local.location_key}${var.name_postfix}"
-  resource_group_name  = azurerm_resource_group.swarm_cluster.name
-  virtual_network_name = azurerm_virtual_network.swarm_cluster_vnet.name
-  address_prefixes     = ["10.254.2.0/24"] # /24 == 251 + 5 Azure reserved addresses
-
-  #   lifecycle {
-  #     prevent_destroy = true
-  #   }
-}
-
-resource "azurerm_availability_set" "vm_availabilityset_manager" {
-  name                         = "${local.product_key}-${local.environment_key}-availabilityset-${local.location_key}${var.name_postfix}"
-  resource_group_name          = azurerm_resource_group.swarm_cluster.name
-  location                     = azurerm_resource_group.swarm_cluster.location
-  tags                         = local.tags
-  platform_update_domain_count = 5
-  platform_fault_domain_count  = local.platform_fault_domain_count
-
-  #   lifecycle {
-  #     prevent_destroy = true
-  #   }
-}
 
 # ------------------------------------------------------------------
 # Virtual Machines - Manager 0
 # This node is used to initialize the swarm
 # ------------------------------------------------------------------
 
-resource "azurerm_network_security_group" "manager_sg" {
-  name                = "${local.product_key}-${local.environment_key}-nic-sg-managers-${local.location_key}${var.name_postfix}"
-  resource_group_name = azurerm_resource_group.swarm_cluster.name
-  location            = azurerm_resource_group.swarm_cluster.location
 
-  tags = local.tags
-
-  # Just for testing
-  security_rule {
-    name                       = "http"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-}
 resource "azurerm_public_ip" "publicip_manager0" {
   name                = "${local.product_key}-${local.environment_key}-publicip-manager0-${local.location_key}${var.name_postfix}"
   resource_group_name = azurerm_resource_group.swarm_cluster.name
@@ -167,20 +107,22 @@ resource "azurerm_virtual_machine_data_disk_attachment" "manager0_data_attachmen
 }
 
 # ------------------------------------------------------------------
-# Virtual Machines - Manager 1
+# Managers 1 - n
 # ------------------------------------------------------------------
 
-resource "azurerm_public_ip" "publicip_manager1" {
-  name                = "${local.product_key}-${local.environment_key}-publicip-manager1-${local.location_key}${var.name_postfix}"
+resource "azurerm_public_ip" "publicip_managers" {
+  count               = local.node_manager_count - 1
+  name                = "${local.product_key}-${local.environment_key}-publicip-manager${count.index + 1}-${local.location_key}${var.name_postfix}"
   resource_group_name = azurerm_resource_group.swarm_cluster.name
   location            = azurerm_resource_group.swarm_cluster.location
   allocation_method   = "Dynamic"
-  domain_name_label   = "${local.product_key}-${local.environment_key}-manager1-${local.location_key}${var.name_postfix}"
+  domain_name_label   = "${local.product_key}-${local.environment_key}-manager${count.index + 1}-${local.location_key}${var.name_postfix}"
   tags                = local.tags
 }
 
-resource "azurerm_network_interface" "nic_manager1" {
-  name                          = "${local.product_key}-${local.environment_key}-nic-manager1-${local.location_key}${var.name_postfix}"
+resource "azurerm_network_interface" "nic_managers" {
+  count                         = local.node_manager_count - 1
+  name                          = "${local.product_key}-${local.environment_key}-nic-manager${count.index + 1}-${local.location_key}${var.name_postfix}"
   resource_group_name           = azurerm_resource_group.swarm_cluster.name
   location                      = azurerm_resource_group.swarm_cluster.location
   enable_accelerated_networking = true
@@ -191,24 +133,26 @@ resource "azurerm_network_interface" "nic_manager1" {
     name                          = "ipconfig1"
     subnet_id                     = azurerm_subnet.node_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.publicip_manager1.id
+    public_ip_address_id          = azurerm_public_ip.publicip_managers[count.index].id
     primary                       = true
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "nic_manager1_sg" {
-  network_interface_id      = azurerm_network_interface.nic_manager1.id
+resource "azurerm_network_interface_security_group_association" "nic_managers_sg" {
+  count                     = local.node_manager_count - 1
+  network_interface_id      = azurerm_network_interface.nic_managers[count.index].id
   network_security_group_id = azurerm_network_security_group.manager_sg.id
 }
 
-resource "azurerm_linux_virtual_machine" "manager1" {
-  name                       = "${local.product_key}-${local.environment_key}-vm-manager1-${local.location_key}${var.name_postfix}"
-  computer_name              = "manager1"
+resource "azurerm_linux_virtual_machine" "managers" {
+  count                      = local.node_manager_count - 1
+  name                       = "${local.product_key}-${local.environment_key}-vm-manager${count.index + 1}-${local.location_key}${var.name_postfix}"
+  computer_name              = "manager${count.index + 1}"
   resource_group_name        = azurerm_resource_group.swarm_cluster.name
   location                   = azurerm_resource_group.swarm_cluster.location
   size                       = local.node_manager_size
   admin_username             = local.vm_admin_username
-  network_interface_ids      = [azurerm_network_interface.nic_manager1.id]
+  network_interface_ids      = [azurerm_network_interface.nic_managers[count.index].id]
   availability_set_id        = azurerm_availability_set.vm_availabilityset_manager.id
   encryption_at_host_enabled = true
   priority                   = "Regular"
@@ -251,8 +195,9 @@ resource "azurerm_linux_virtual_machine" "manager1" {
   depends_on = [azurerm_storage_account.cluster_storage, azurerm_linux_virtual_machine.manager0]
 }
 
-resource "azurerm_managed_disk" "manager1_data" {
-  name                 = "manager1-data"
+resource "azurerm_managed_disk" "managers_data" {
+  count                = local.node_manager_count - 1
+  name                 = "manager${count.index + 1}1-data"
   location             = azurerm_resource_group.swarm_cluster.location
   resource_group_name  = azurerm_resource_group.swarm_cluster.name
   storage_account_type = "Premium_LRS"
@@ -263,8 +208,9 @@ resource "azurerm_managed_disk" "manager1_data" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "manager1_data_attachment" {
-  managed_disk_id    = azurerm_managed_disk.manager1_data.id
-  virtual_machine_id = azurerm_linux_virtual_machine.manager1.id
+  count              = local.node_manager_count - 1
+  managed_disk_id    = azurerm_managed_disk.managers_data[count.index].id
+  virtual_machine_id = azurerm_linux_virtual_machine.managers[count.index].id
   lun                = "0"
   caching            = "ReadWrite"
 }
@@ -272,41 +218,6 @@ resource "azurerm_virtual_machine_data_disk_attachment" "manager1_data_attachmen
 # ------------------------------------------------------------------
 # Worker node
 
-resource "azurerm_network_security_group" "worker_sg" {
-  name                = "${local.product_key}-${local.environment_key}-nic-sg-workers-${local.location_key}${var.name_postfix}"
-  resource_group_name = azurerm_resource_group.swarm_cluster.name
-  location            = azurerm_resource_group.swarm_cluster.location
-
-  tags = local.tags
-
-  # # Just for testing
-  # security_rule {
-  #   name                       = "http"
-  #   priority                   = 100
-  #   direction                  = "Inbound"
-  #   access                     = "Allow"
-  #   protocol                   = "Tcp"
-  #   source_port_range          = "*"
-  #   destination_port_range     = "*"
-  #   source_address_prefix      = "*"
-  #   destination_address_prefix = "*"
-  # }
-
-}
-
-
-resource "azurerm_availability_set" "vm_availabilityset_workers" {
-  name                         = "${local.product_key}-${local.environment_key}-availabilityset-worker-${local.location_key}${var.name_postfix}"
-  resource_group_name          = azurerm_resource_group.swarm_cluster.name
-  location                     = azurerm_resource_group.swarm_cluster.location
-  tags                         = local.tags
-  platform_update_domain_count = 5
-  platform_fault_domain_count  = local.platform_fault_domain_count
-
-  #   lifecycle {
-  #     prevent_destroy = true
-  #   }
-}
 
 resource "azurerm_public_ip" "publicip_workers" {
   count               = local.node_worker_count
---
+++ main-swarm-network.tf
@@ -0,0 +1,102 @@
+resource "azurerm_resource_group" "swarm_cluster" {
+  name     = "${local.product_key}-${local.environment_key}-${local.location_key}-swarm-rg"
+  location = local.location
+  tags     = local.tags
+}
+
+
+resource "azurerm_virtual_network" "swarm_cluster_vnet" {
+  name                = "${local.product_key}-${local.environment_key}-vnet-${local.location_key}${var.name_postfix}"
+  resource_group_name = azurerm_resource_group.swarm_cluster.name
+  location            = azurerm_resource_group.swarm_cluster.location
+  address_space       = ["10.254.0.0/16"] # /16 == 65,536 addresses
+  tags                = local.tags
+
+  #   lifecycle {
+  #     prevent_destroy = true
+  #   }
+}
+
+resource "azurerm_subnet" "node_subnet" {
+  name                 = "${local.product_key}-${local.environment_key}-subnet-backend-${local.location_key}${var.name_postfix}"
+  resource_group_name  = azurerm_resource_group.swarm_cluster.name
+  virtual_network_name = azurerm_virtual_network.swarm_cluster_vnet.name
+  address_prefixes     = ["10.254.2.0/24"] # /24 == 251 + 5 Azure reserved addresses
+
+  #   lifecycle {
+  #     prevent_destroy = true
+  #   }
+}
+
+resource "azurerm_availability_set" "vm_availabilityset_manager" {
+  name                         = "${local.product_key}-${local.environment_key}-availabilityset-${local.location_key}${var.name_postfix}"
+  resource_group_name          = azurerm_resource_group.swarm_cluster.name
+  location                     = azurerm_resource_group.swarm_cluster.location
+  tags                         = local.tags
+  platform_update_domain_count = 5
+  platform_fault_domain_count  = local.platform_fault_domain_count
+
+  #   lifecycle {
+  #     prevent_destroy = true
+  #   }
+}
+
+
+resource "azurerm_availability_set" "vm_availabilityset_workers" {
+  name                         = "${local.product_key}-${local.environment_key}-availabilityset-worker-${local.location_key}${var.name_postfix}"
+  resource_group_name          = azurerm_resource_group.swarm_cluster.name
+  location                     = azurerm_resource_group.swarm_cluster.location
+  tags                         = local.tags
+  platform_update_domain_count = 5
+  platform_fault_domain_count  = local.platform_fault_domain_count
+
+  #   lifecycle {
+  #     prevent_destroy = true
+  #   }
+}
+
+# Network security rules for managers
+resource "azurerm_network_security_group" "manager_sg" {
+  name                = "${local.product_key}-${local.environment_key}-nic-sg-managers-${local.location_key}${var.name_postfix}"
+  resource_group_name = azurerm_resource_group.swarm_cluster.name
+  location            = azurerm_resource_group.swarm_cluster.location
+
+  tags = local.tags
+
+  # Just for testing
+  security_rule {
+    name                       = "http"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}
+
+# Network security rules for workers
+resource "azurerm_network_security_group" "worker_sg" {
+  name                = "${local.product_key}-${local.environment_key}-nic-sg-workers-${local.location_key}${var.name_postfix}"
+  resource_group_name = azurerm_resource_group.swarm_cluster.name
+  location            = azurerm_resource_group.swarm_cluster.location
+
+  tags = local.tags
+
+  # # Just for testing
+  # security_rule {
+  #   name                       = "http"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "*"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
+
+}
