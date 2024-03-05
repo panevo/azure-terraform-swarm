05d4139c1f9d0f351082f6738f15e045aec1d7c2
Thomas Tartière <thomas.tartiere@gmail.com>
2024-03-05 11:29:56 -0800

    feat: Mount data disk

--- data.tf
+++ data.tf
@@ -30,13 +30,24 @@ data "template_cloudinit_config" "manager_config" {
   }
 }
 
-data "cloudinit_config" "worker_config" {
+
+data "template_file" "worker_config_file" {
+  template = file("${path.module}/cloud-config/worker.yml")
+
+  vars = {
+    storage_account_name = azurerm_storage_account.cluster_storage.name
+    storage_account_key  = azurerm_storage_account.cluster_storage.primary_access_key
+    username             = local.vm_admin_username
+  }
+}
+
+data "template_cloudinit_config" "worker_config" {
   gzip          = true
   base64_encode = true
 
   # Main cloud-config configuration file.
   part {
     content_type = "text/cloud-config"
-    content      = file("${path.module}/cloud-config/worker.yml")
+    content      = data.template_file.worker_config_file.rendered
   }
 }
